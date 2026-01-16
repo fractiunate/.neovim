555bae5dfb0ee73050ecc10da59efb21d06e9e56
Fractiunate <fractiunate@gmail.com>
2026-01-16 17:00:38 +0100

    fix: add neo-tree

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,11 @@ vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 2
 
+-- Diagnostics: show full error text inline
+vim.diagnostic.config({
+  virtual_text = true,       -- show message at end of line
+  signs = true,              -- show E/W/I/H in sign column
+  underline = true,          -- underline the error
+  update_in_insert = false,  -- don't update while typing
+  severity_sort = true,      -- show most severe first
+})
